5ab9f2a0bc74f4ac60e4f871b816dccb62a38f90
jeffxo419 <godlikejeff@gmail.com>
2020-10-08 10:26:41 +0800

    one

--- NotificationIOS/AppDelegate.swift
+++ NotificationIOS/AppDelegate.swift
@@ -7,16 +7,30 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate , UNUserNotificationCenterDelegate{
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+           
+           // 在程式一啟動即詢問使用者是否接受圖文(alert)、聲音(sound)、數字(badge)三種類型的通知
+           UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge, .carPlay], completionHandler: { (granted, error) in
+               if granted {
+                   print("允許")
+               } else {
+                   print("不允許")
+               }
+           })
+        
+        // 代理 UNUserNotificationCenterDelegate，這麼做可讓 App 在前景狀態下收到通知
+           UNUserNotificationCenter.current().delegate = self
+        
+
+           return true
+       }
 
     // MARK: UISceneSession Lifecycle
 
@@ -30,6 +44,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    // 在前景收到通知時所觸發的 function
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("在前景收到通知...")
+        completionHandler([.badge, .sound, .alert])
+    }
+    
+    
+    
+    //點擊通知後觸發
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler:  @escaping () -> Void) {
+        let content: UNNotificationContent = response.notification.request.content
+        
+        completionHandler()
+        
+        // 取出userInfo的link並開啟Facebook
+        let requestUrl = URL(string: content.userInfo["link"]! as! String)
+        UIApplication.shared.open(requestUrl!, options: [:], completionHandler: nil)
     }
 
 
--- NotificationIOS/ViewController.swift
+++ NotificationIOS/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func notify(_ sender: UIButton) {
+        let content = UNMutableNotificationContent() //通知標題＋副標＋內容
+        content.title = "通知"
+        content.subtitle = "ios"
+        content.body = "今天是禮拜三"
+        content.sound = UNNotificationSound.default
+        content.badge = 1
+        
+        
+        
+      /*  let tempDirURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory:  true)//回傳暫存檔目錄
+        
+        let tempFileURL = tempDirURL.appendingPathComponent("suggested-apple.jpg")
+      */
+        
+        let imageURL: URL = Bundle.main.url(forResource: "apple", withExtension: "png")!  //取得bundle物件
+        let attachment = try! UNNotificationAttachment(identifier: "image", url: imageURL, options: nil)
+        
+        content.attachments = [attachment]
+        
+         content.userInfo = ["link" : "https://www.google.com/?client=safari&channel=mac_bm"]
+        
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10 , repeats: false)//觸發器
+        let request = UNNotificationRequest(identifier: "1", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler:{
+            error in  print("成功建立通知...")
+        }) //排定通知
+    }
+    
+    
 }
 
